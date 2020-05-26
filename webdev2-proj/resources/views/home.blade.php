15a0cff770900712bebb53d375e79cc09a81a728
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2020-05-26 21:33:29 +0200

    finished desktop home

--- webdev2-proj/resources/views/home.blade.php
+++ webdev2-proj/resources/views/home.blade.php
@@ -21,4 +21,28 @@
         </div>
     </section>
 
+    <section class="splash">
+        <img src="{{asset('/images/sec2.jpg')}}">
+        <div class="right">
+            <div class="container">
+                <div class="text">
+                    <h2>RUNESCAPE MOBILE EARLY ACCESS</h2>
+                    <p>RuneScape Mobile Early Access is now available for all RuneScape players on Android.</p>
+                    <p> Early Access is your chance to experience the magic of RuneScape on your mobile and with membership, grab the exclusive Mobile Founder's Pack. </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="newsletter">
+        <div class="container">
+            <form action="">
+                @csrf
+                <label for="email">Sign up for our newsletter</label><br>
+                <input type="email" name="email" placeholder="someone@example.com">
+                <input class="test" type="submit" value="SIGN UP">
+            </form>
+        </div>
+    </section>
+
 @endsection
