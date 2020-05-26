488315331992b42249de09b74fef71af9198a335
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2020-05-26 20:36:34 +0200

    fixed the splash text center issue

--- webdev2-proj/resources/views/home.blade.php
+++ webdev2-proj/resources/views/home.blade.php
@@ -3,17 +3,22 @@
 @section('content')
     <section class="attention">
         <img src="{{asset('/images/sec0.jpg')}}">
-        <div>
+        <div class="title">
             <h1>RUNESCAPE ON<br><span>MOBILE</span></h1>
         </div>
     </section>
 
     <section class="splash">
         <img src="{{asset('/images/sec1.jpg')}}">
-        <div class="left container">
-            <h2>RUNESCAPE MOBILE EARLY ACCESS</h2>
-            <p>RuneScape Mobile Early Access is now available for all RuneScape players on Android.</p>
-            <p> Early Access is your chance to experience the magic of RuneScape on your mobile and with membership, grab the exclusive Mobile Founder's Pack. </p>
+        <div class="left">
+            <div class="container">
+                <div class="text">
+                    <h2>RUNESCAPE MOBILE EARLY ACCESS</h2>
+                    <p>RuneScape Mobile Early Access is now available for all RuneScape players on Android.</p>
+                    <p> Early Access is your chance to experience the magic of RuneScape on your mobile and with membership, grab the exclusive Mobile Founder's Pack. </p>
+                </div>
+            </div>
         </div>
     </section>
+
 @endsection
