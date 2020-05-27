2af9894a4d2c4f9815f75ca3bc732bf8a4fd33f2
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2020-05-27 11:39:35 +0200

    added favicon

--- webdev2-proj/resources/views/partials/header.blade.php
+++ webdev2-proj/resources/views/partials/header.blade.php
@@ -7,15 +7,24 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Runescape mobile</title>
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/branding/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/branding/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/branding/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('images/branding/site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ asset('images/branding/safari-pinned-tab.svg') }}" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
 </head>
 <body>
     <header>
         <div class="toolbar">
             <div class="container">
                 <ul>
-                    <li><a href="">Donate</a></li>
-                    <li><a href="">Contact</a></li>
-                    <li><a class="cta" href="">Login</a></li>
+                    <li class="lang"><a @if(App::getLocale() == 'en') class="active" @endif href="{{ route(Route::currentRouteName(), 'en') }}">EN</a></li>
+                    <li class="lang"><a @if(App::getLocale() == 'nl') class="active" @endif href="{{ route(Route::currentRouteName(), 'nl') }}">NL</a></li>  
+                    <li><a href="">{{ __('header.donate') }}</a></li>
+                    <li><a href="">{{ __('header.contact') }}</a></li>
+                    <li><a class="cta" href="">{{ __('header.login') }}</a></li>
                 </ul>
             </div>
         </div>
@@ -23,9 +32,9 @@
             <div class="container">
                 <ul>
                     <li class="logo" ><a href="">Runescape</a></li>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">News blogs</a></li>
-                    <li><a href="">About</a></li>
+                    <li><a href="">{{ __('header.home') }}</a></li>
+                    <li><a href="">{{ __('header.news') }}</a></li>
+                    <li><a href="">{{ __('header.about') }}</a></li>
                 </ul>
             </div>
         </nav>
