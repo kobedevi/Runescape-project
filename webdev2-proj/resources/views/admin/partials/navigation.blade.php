ed2319f8cf11934f13afbe991c3a0fd8b3d2f8ab
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2020-06-04 03:35:20 +0200

    admin navigation seperation

--- webdev2-proj/resources/views/admin/partials/navigation.blade.php
+++ webdev2-proj/resources/views/admin/partials/navigation.blade.php
@@ -4,6 +4,9 @@
         <a href="{{route('newsAdmin', app()->getLocale())}}"><li>{{ __('admin.news') }}</li></a>
         <a href="{{route('about.edit', app()->getLocale())}}"><li>{{ __('admin.about') }}</li></a>
         <a href="{{route('privacy.edit', app()->getLocale())}}"><li>{{ __('admin.privacy') }}</li></a>
+        
+    </ul>
+    <ul class="bottom">
         <a href="{{route('donations.read', app()->getLocale())}}"><li>{{ __('admin.donations') }}</li></a>
         <a href="{{route('users', app()->getLocale())}}"><li>Users</li></a>
     </ul>
