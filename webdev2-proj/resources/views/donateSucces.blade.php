6b6f75ccdff8f4b2b75ee025b99bcd6d60964b2a
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2020-06-05 22:40:25 +0200

    made error pages responsive

--- webdev2-proj/resources/views/donateSucces.blade.php
+++ webdev2-proj/resources/views/donateSucces.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Runescape Mobile - Thank you</title>
     <link rel="stylesheet" href="{{ mix('/css/error.css') }}">
+    <link rel="stylesheet" href="{{ mix('/css/responsive.css') }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/branding/apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/branding/favicon-32x32.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/branding/favicon-16x16.png') }}">
@@ -14,10 +15,10 @@
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">  
 </head>
-<body>
+<body class="errorContainer">
     <div>
         <img src="{{ asset('/images/branding/rs.png') }}">
-        <h1>{{__('donate.succes.thanks')}}</h1>
+        <h1 class="errorTitle">{{__('donate.succes.thanks')}}</h1>
         <p>{{__('donate.succes.info')}}</p>
         <a href="{{ route('donate', app()->getLocale()) }}">{{__('donate.succes.back')}}</a>
     </div>
--- webdev2-proj/resources/views/errors/404.blade.php
+++ webdev2-proj/resources/views/errors/404.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Runescape Mobile - Not Found</title>
     <link rel="stylesheet" href="{{ mix('/css/error.css') }}">
+    <link rel="stylesheet" href="{{ mix('/css/responsive.css') }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/branding/apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/branding/favicon-32x32.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/branding/favicon-16x16.png') }}">
@@ -14,11 +15,11 @@
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">  
 </head>
-<body>
+<body class="errorContainer">
     <div>
         <img src="{{ asset('/images/branding/rs.png') }}">
         <h2>{{ $exception->getStatusCode() }}</h2>
-        <h1>{{ __('alert.error.title')}}</h1>
+        <h1 class="errorTitle">{{ __('alert.error.title')}}</h1>
         <p>{!! __('alert.error.info') !!}</p>
     <a href="{{ url()->previous() }}">{{ __('alert.error.back')}}</a>
     </div>
