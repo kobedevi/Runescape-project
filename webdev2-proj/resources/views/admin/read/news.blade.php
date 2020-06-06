fcb8157eaed2c43e40e7f8cbe251b115137e192a
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2020-06-06 17:15:30 +0200

    reworked news admin page

--- webdev2-proj/resources/views/admin/read/news.blade.php
+++ webdev2-proj/resources/views/admin/read/news.blade.php
@@ -1,21 +1,31 @@
 @extends('admin.layout')
 
 @section('content')
-<h1>{{ __('admin.news') }}</h1>
-<ul>
-    <a class="add button" href="{{ route('newsAdmin.add', app()->getLocale())}}">{{__('admin.add')}}</a>
-    @foreach ( $posts as $post)
-        <li class="listitem">
-            <div>
-                <h3>{{ $post->{'title_'.App::getLocale()} }}</h3>
-                <img src="{{ asset('/images/news/'. $post->image) }}">
-                <p>{{ $post->{'text_'.App::getLocale()}  }}</p>
-            </div>
-            <div class="buttons">
-                <a class="edit button" href="{{ route('newsAdmin.edit', [app()->getLocale(), $post->id])}}">{{__('admin.edit')}}</a>
-                <a class="delete button" href="{{ route('newsAdmin.destroy', ['language' => app()->getLocale(), 'id' => $post->id]) }}">{{__('admin.delete')}}</a>  
-            </div>
-        </li>
-    @endforeach
-</ul>
+<table>
+    <thead>
+        <tr>
+            <th>Titel</th>
+            <th>Intro</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ( $posts as $post)
+            <tr>
+                <td>
+                    <div>
+                        <h3>{{  $post->{'title_'.App::getLocale()} }}</h3>
+                        <img src="{{ asset('/images/news/'. $post->image) }}">
+                    </div>
+                </td>
+                <td class="intro">{{ Str::limit($post->{'intro_'.App::getLocale()}, 200) }}</td>
+                <td class="buttons">
+                    <a class="edit button" href="{{ route('pages.edit', [app()->getLocale(), $post->id])}}">{{__('admin.edit')}}</a>
+                    <a class="delete button" href="{{ route('pages.destroy', ['language' => app()->getLocale(), 'id' => $post->id]) }}">{{__('admin.delete')}}</a>  
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
 @endsection
